a5166a2f73ba5fc9c2ba0d531cd2d41d0a4552c4
erl-j <nicolasjonason@gmail.com>
2024-04-17 08:21:19 +0200

    update bfn test

--- slm/bfn_test.py
+++ slm/bfn_test.py
@@ -3,16 +3,24 @@
 device = "cuda:3"
 from bfn import BFNModel
 
-checkpoint = "../checkpoints/wobbly-river-40/epoch=0-step=10375-val/loss_epoch=0.01564.ckpt"
+checkpoint = "../checkpoints/wobbly-river-40/epoch=2-step=31125-val/loss_epoch=0.00729.ckpt"
 
 model = BFNModel.load_from_checkpoint(checkpoint, map_location=device)
 # %%
 
-
-
 tokenizer = model.tokenizer
 
-y = model.sample(10,20,device=device, plot_interval=5)
+mask = tokenizer.constraint_mask(
+    scale="C major",
+    instruments = ["Piano","Drums","Bass"],
+    min_notes = 50,
+    max_notes = 100,
+)
+
+
+BATCH_SIZE = 10
+N_STEPS = 20
+y = model.sample(None,BATCH_SIZE,N_STEPS,device=device,argmax=False)
 
 import matplotlib.pyplot as plt
 import torch
@@ -21,11 +29,21 @@ y1h = torch.nn.functional.one_hot(y, num_classes=len(model.tokenizer.vocab)).flo
 plt.imshow(y1h[0].cpu().numpy().T, aspect="auto",interpolation="none")
 plt.show()
 
-from util import preview
+from util import preview, piano_roll
 
-y_sm = model.tokenizer.decode(y[0])
+# plot piano rolls,
+# use a 16:9 aspect ratio for each plot
+# subplots
+fig, axs = plt.subplots(BATCH_SIZE,1, figsize=(4,2*BATCH_SIZE))
+for i in range(BATCH_SIZE):        
+    y_sm = model.tokenizer.decode(y[i])
+    # print number of notes
+    print(f"Number of notes: {y_sm.note_num()}")
 
-print(f"number of notes: {y_sm.note_num()}")
+    pr = piano_roll(y_sm, tpq=4)
+    axs[i].imshow(pr, aspect="auto",interpolation="none")
+plt.show()
 
-preview(y_sm, "artefacts/tmp.mid")
-# %%
+# play audio of last 
+preview(y_sm, tmp_dir="tmp", audio=True)
+    
